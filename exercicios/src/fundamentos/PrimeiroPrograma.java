15ba93cd28a5cdc3b71353b2f215514f28ee8554
Fabiola Rocha <fabiolagrocha@outlook.com.br>
2022-07-16 17:48:31 -0300

    First commit

---
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,9 @@
+package fundamentos;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		System.out.println("Meu primeiro programa na CODER - Parte I!!!");
+		System.out.println("Meu primeiro programa na CODER - Parte II!!!");
+	}
+}
